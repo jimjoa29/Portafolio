0cba46ecf28ae8d1e8b7efea11377470bc9b94d7
Joan <jimjoa2903@gmail.com>
2023-06-21 20:16:02 -0400

    corrigiendo errores de imagenes

--- src/components/BotonWhatsapp.jsx
+++ src/components/BotonWhatsapp.jsx
@@ -4,7 +4,7 @@ const BotonWhatsapp = () => {
   return (
     <div>
         {/* Boton de Whatsapp */}
-        <a href="https://wa.link/4sx8is"><img src="../../public/img/btn-whatsapp.png" alt="logo-whatsapp" 
+        <a href="https://wa.link/4sx8is"><img src="public/img/btn-whatsapp.png" alt="logo-whatsapp" 
         className='fixed bottom-5 right-10 w-14 fixer'/></a>
     </div>
   )
