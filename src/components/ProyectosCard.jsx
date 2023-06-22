28962b8d642290ef3fc332977656ef7e1ee6640d
Joan <jimjoa2903@gmail.com>
2023-06-22 00:10:49 -0400

    Comrrigiendo tamaños de imagenes de proyectos

--- src/components/ProyectosCard.jsx
+++ src/components/ProyectosCard.jsx
@@ -1,9 +1,9 @@
 const ProyectosCard = ({ titulo, view, imagen, url}) => {
   return (
-    <div className="w-9/12">
+    <div className="w-[95%]">
       <div className="text-white text-center ">
         <a href={url}>
-          <img src={imagen} alt="" className="w-72 rounded-md hover:-translate-y-2.5 transition-all" />
+          <img src={imagen} alt="" className="md:w-full lg: w-72 rounded-md hover:-translate-y-2.5 transition-all" />
         </a>
         
         <h2 className="pt-4 text-2xl  text-teal-500">{titulo}</h2>
